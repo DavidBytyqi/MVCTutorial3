4f408e9a680f291c0420229b4722bfa69cef1e48
davidb <davidb@APPDEC-PC>
2017-06-15 10:20:42 +0200

    Read Details

---
+++ MVCTutorial3/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using MVCTutorial3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial3.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+
+            MVCTutorial2Entities db = new MVCTutorial2Entities();
+            List<Employee> list = db.Employees.ToList();
+            EmployeeViewModel em = new EmployeeViewModel();
+            List<EmployeeViewModel> employee = list.Select(x => new EmployeeViewModel
+            {
+               
+                Emri = x.Emri,
+                EmployeeId=x.EmployeeId
+                
+            }).ToList();
+            return View(employee);
+        }
+        public ActionResult EmployeeDetail(int EmployeeId)
+        {
+            MVCTutorial2Entities db = new MVCTutorial2Entities();
+            Employee em = db.Employees.SingleOrDefault(x => EmployeeId == x.EmployeeId);
+            EmployeeViewModel employee = new EmployeeViewModel();
+            employee.Emri = em.Emri;
+            employee.EmployeeId = em.EmployeeId;
+            employee.Address = em.Address;
+            employee.DepartmentId = em.DepartmentId;
+            employee.DepartmentName = em.Department.DepartmentName;
+
+            return View(employee);
+
+        }
+    }
+}
---
+++ MVCTutorial3/Models/EmployeeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTutorial3.Models
+{
+    public class EmployeeViewModel
+    {
+
+        public int EmployeeId { get; set; }
+        public string Emri { get; set; }
+        public int DepartmentId { get; set; }
+        public string Address { get; set; }
+        public string DepartmentName { get; set; }
+
+        public virtual Department Department { get; set; }
+    }
+}
